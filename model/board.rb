bc0f36588c991c7e6247131fcf59c2fcedf78f5c
abe <abeh9197@gmail.com>
2024-06-02 21:11:00 +0900

    add: can_flank, search_direction funx

--- model/board.rb
+++ model/board.rb
@@ -14,7 +14,7 @@ class Board
     @value[input[0]][input[1]] = game.turn
   end
 
-  def valid_moves(board, game)
+  def valid_moves(game)
     moves = []
     @value.each_with_index do |row, y|
       row.each_with_index do |cell, x|
@@ -31,5 +31,31 @@ class Board
     @value[4][4] = 1
     @value[3][4] = 2
     @value[4][3] = 2
+  end
+
+  def can_flank?(y, x, turn)
+    directions = [
+      [-1, -1], [-1, 0], [-1, 1],
+      [0, 1],[0, 1],
+      [1, -1], [1, 0], [1, 1]
+    ]
+
+    directions.any? do |dy, dx|
+      next_y, next_x = y + dy, x + dx
+      in_bounds?(next_y, next_x) && @value[next_y][next_x] != turn && @value[next_y][next_x] != nil && search_direction(next_y, next_x, dy, dx, turn)
+    end
+  end
+
+  def search_direction(y, x, dy, dx, turn, initial: true)
+    next_y, next_x = y + dy, x + dx
+    return false unless in_bounds?(next_y, next_x)
+    return true if @value[next_y][next_x] == turn
+    return false if @value[next_y][next_x] == nil?
+
+    search_direction(next_y, next_x, dy, dx, turn, initial: false)
+  end
+
+  def in_bounds?(y, x)
+    y.between?(0, @range - 1) && x.between?(0, @range - 1)
   end
 end
